2869704c02ee3e45b70e6e10f1adeabe0efb83a5
Michael Gauthier <gauthiermichael3.14@gmail.com>
2023-07-29 18:53:54 -0500

    WIP

--- AML/AMLVector3.cpp
+++ AML/AMLVector3.cpp
@@ -4,22 +4,70 @@
 namespace AML
 {
 	// Constructors
-	Vector3::Vector3(){}
-	Vector3::Vector3(double val){}
+	Vector3::Vector3()
+	:x(0.0), y(0.0), z(0.0)
+	{}
+	Vector3::Vector3(double val)
+	:x(val), y(val), z(val)
+	{}
 	Vector3::Vector3(double x, double y, double z){}
-	Vector3::Vector3(const double data[3]){}
+	:x(x_), y(y_), z(z_)
+	{}
+	Vector3::Vector3(const double data_[3])
+	:x(data_[0]), y(data_[1], z(data_[2])
+	{}
 
 	// Operator Assignments (Vector)
-	Vector3& Vector3::operator+=(const Vector3& rhs){}
-	Vector3& Vector3::operator-=(const Vector3& rhs){}
-	Vector3& Vector3::operator*=(const Vector3& rhs){}
-	Vector3& Vector3::operator/=(const Vector3& rhs){}
+	Vector3& Vector3::operator+=(const Vector3& rhs)
+	{
+		x += rhs.x;
+		y += rhs.y;
+		z += rhs.z;
+	}
+	Vector3& Vector3::operator-=(const Vector3& rhs)
+	{
+		x -= rhs.x;
+		y -= rhs.y;
+		z -= rhs.z;
+	}
+	Vector3& Vector3::operator*=(const Vector3& rhs)
+	{
+		x *= rhs.x;
+		y *= rhs.y;
+		z *= rhs.z;
+	}
+	Vector3& Vector3::operator/=(const Vector3& rhs)
+	{
+		x /= rhs.x;
+		y /= rhs.y;
+		z /= rhs.z;
+	}
 
 	// Operator Assignments (Scalar)
-	Vector3& Vector3::operator+=(double s){}
-	Vector3& Vector3::operator-=(double s){}
-	Vector3& Vector3::operator*=(double s){}
-	Vector3& Vector3::operator/=(double s){}
+	Vector3& Vector3::operator+=(double s)
+	{
+		x += s;
+		y += s;
+		z += s;
+	}
+	Vector3& Vector3::operator-=(double s)
+	{
+		x -= s;
+		y -= s;
+		z -= s;
+	}
+	Vector3& Vector3::operator*=(double s)
+	{
+		x *= s;
+		y *= s;
+		z *= s;
+	}
+	Vector3& Vector3::operator/=(double s)
+	{
+		x /= s;
+		y /= s;
+		z /= s;
+	}
 
 	// Special Object Creators
 	static const Vector3::Vector3 xAxis(){}
