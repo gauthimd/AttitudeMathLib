fbf5a61d269c7dbbafd142327cdfff6718ad92e4
Michael Gauthier <gauthiermichael3.14@gmail.com>
2023-07-29 17:23:31 -0500

    First commit

---
+++ example/main.cpp
@@ -0,0 +1,13 @@
+// System includes
+#include <iostream>
+#include <string>
+#include <cmath>
+
+//Program includes
+#include "AML.h"
+using namespace AML;
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
