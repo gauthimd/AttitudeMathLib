e9b9fe3fc74ad4a8c163a567d086595e8cbb2e96
Michael Gauthier <gauthiermichael3.14@gmail.com>
2023-07-30 12:08:13 -0500

    finished AMLVector3

--- AML/AMLVector3.cpp
+++ AML/AMLVector3.cpp
@@ -70,35 +70,57 @@ namespace AML
 	}
 
 	// Special Object Creators
-	static const Vector3::Vector3 xAxis(){}
-	static const Vector3::Vector3 yAxis(){}
-	static const Vector3::Vector3 zAxis(){}
+	static const Vector3::Vector3 xAxis(){return Vector3(1.0, 0.0, 0.0);}
+	static const Vector3::Vector3 yAxis(){return Vector3(0.0, 1.0, 0.0);}
+	static const Vector3::Vector3 zAxis(){return Vector3(0.0, 0.0, 1.0);}
 
 	// Vector / Vector Elementwise Operations
-	Vector3 operator-(const Vector3& rhs){}
-	Vector3 operator+(const Vector3& lhs, const Vector3& rhs){}
-	Vector3 operator-(const Vector3& lhs, const Vector3& rhs){}
-	Vector3 operator*(const Vector3& lhs, const Vector3& rhs){}
-	Vector3 operator/(const Vector3& lhs, const Vector3& rhs){}
+	Vector3 operator-(const Vector3& rhs){return Vector3(-rhs.x, -rhs.y, -rhs.z);}
+	Vector3 operator+(const Vector3& lhs, const Vector3& rhs){return (Vector3(lhs) += rhs);}
+	Vector3 operator-(const Vector3& lhs, const Vector3& rhs){return (Vector3(lhs) -= rhs);}
+	Vector3 operator*(const Vector3& lhs, const Vector3& rhs){return (Vector3(lhs) *= rhs);}
+	Vector3 operator/(const Vector3& lhs, const Vector3& rhs){return (Vector3(lhs) /= rhs);}
 	
 	// Vector / Scalar Operations
-	Vector3 operator+(const Vector3& lhs, double s){}
-	Vector3 operator-(const Vector3& lhs, double s){}
-	Vector3 operator*(const Vector3& lhs, double s){}
-	Vector3 operator/(const Vector3& lhs, double s){}
-	Vector3 operator+(double s, const Vector3& rhs){}
-	Vector3 operator-(double s, const Vector3& rhs){}
-	Vector3 operator*(double s, const Vector3& rhs){}
-	Vector3 operator/(double s, const Vector3& rhs){}
+	Vector3 operator+(const Vector3& lhs, double s){return (Vector3(lhs) += s);}
+	Vector3 operator-(const Vector3& lhs, double s){return (Vector3(lhs) -= s);}
+	Vector3 operator*(const Vector3& lhs, double s){return (Vector3(lhs) *= s);}
+	Vector3 operator/(const Vector3& lhs, double s){return (Vector3(lhs) /= s);}
+	Vector3 operator+(double s, const Vector3& rhs){return (Vector3(s) += rhs);}
+	Vector3 operator-(double s, const Vector3& rhs){return (Vector3(s) -= rhs);}
+	Vector3 operator*(double s, const Vector3& rhs){return (Vector3(s) *= rhs);}
+	Vector3 operator/(double s, const Vector3& rhs){return (Vector3(s) /= rhs);}
 	
 	// Vector Operations
-	double norm(const Vector3& rhs){}
-	void normalize(Vector3& rhs){}
-	Vector3 unit(const Vector3& rhs){}
-	Vector3 cross(const Vector3& lhs, const Vector3& rhs){}
-	double dot(const Vector3& lhs, const Vector3& rhs){}
+	double norm(const Vector3& rhs){return sqrt(rhs.x*rhs.x + rhs.y*rhs.y + rhs.z*rhs.z);}
+	void normalize(Vector3& rhs)
+	{
+		double mag = norm(rhs);
+		if(mag > 0.0){rhs /= mag;}
+	}
+	Vector3 unit(const Vector3& rhs)
+	{
+		double mag = norm(rhs);
+		if(mag > 0.0){return (Vector3(rhs)/mag);}
+		return Vector3(rhs);
+	}
+	Vector3 cross(const Vector3& lhs, const Vector3& rhs)
+	{
+		double x = (lhs.y * rhs.z) - (lhs.z* rhs.y);
+		double y = (lhs.z * rhs.x) - (lhs.x* rhs.z);
+		double z = (lhs.x * rhs.y) - (lhs.y* rhs.x);
+		return Vector3(x,y,z);
+	}
+	double dot(const Vector3& lhs, const Vector3& rhs)
+	{
+		return (rhs.x*lhs.x + rhs.y*lhs.y + rhs.z*lhs.z);
+	}
 	
 	// Stream Functions
-	std::ostream& operator<<(std::ostream& os, const Vector3& obj){}
+	std::ostream& operator<<(std::ostream& os, const Vector3& obj)
+	{
+		os << "[" << obj.x << ", " << obj.y << ", " obj.z << "]";
+		return os;
+	}
 
 };
--- example/main.cpp
+++ example/main.cpp
@@ -9,5 +9,13 @@ using namespace AML;
 
 int main(int argc, char **argv)
 {
+	Vector3 v1(1.0, 2.0, 3.0);
+	Vector3 v2(4.0, 5.0, 6.0);
+
+	Vector3 v3 = v1 + v2;
+
+	std::cout << v1 << std::endl;
+	std::cout << v2 << std::endl;
+	std::cout << v3 << std::endl;
 	return 0;
 }
